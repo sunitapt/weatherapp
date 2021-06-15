b6a5fa987d062fa33d911b2a8f048a42127189d5
sunita <sunitapt>
2021-06-15 22:07:37 +0530

    herokus setup

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 const app = express()
-
+const port = process.env.PORT || 3000
 //define paths for Express config
 const publicDirectoryPath = path.join(__dirname,'../public')
 const viewsPath = path.join(__dirname,'../templates/views')
@@ -93,7 +93,7 @@ app.get('*',(req,res)=>{
     })
 })
 
-app.listen(3000,()=>{
-    console.log('Server started on port 3000!')
+app.listen(port,()=>{
+    console.log('Server started on port'+port)
 })
 //app.com
